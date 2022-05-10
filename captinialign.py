b811696bd7474b8ef9a9eb0660520c5ed1a56b8f
CatiR <richtercati@gmail.com>
2022-05-10 13:44:38 +0000

    initial commit

---
+++ captinialign.py
@@ -0,0 +1,309 @@
+import shutil, os, subprocess, typing
+from random import choice
+from collections import defaultdict
+
+
+class AlignOneFunction():
+
+    def __init__(self,exercise_text,rec_speaker_id,rec_file_id,rec_duration):
+        self.work = './alignment/new/'
+        self.wav_dir = './demo-wav/'
+        self.mfa_dir = './alignment/capt1ni_pretrained_aligner/'
+        self.pdict_name = '1_combined_added_lexicon'
+        self.log_path = self.work+'log.txt'
+        self.silence_phone = "sil"
+        self.silence_word = "<eps>" # !! TODO
+        
+        self.rec_id = rec_file_id
+        self.speaker_id = rec_speaker_id
+        self.normalised_text = exercise_text
+        self.duration = rec_duration
+        self.long_id = f'{self.speaker_id}-{self.rec_id}'
+        
+        # files already exist:
+        self.disambig_path = self.mfa_dir+'dictionary/phones/disambiguation_symbols.int'
+        self.word_boundary_int = self.mfa_dir+'dictionary/phones/word_boundary.int'
+        self.phone_map_path = self.mfa_dir+'dictionary/phones/phones.txt'
+        self.tree_path = self.mfa_dir+'pretrained_aligner/tree'
+        self.model_path = self.mfa_dir+'pretrained_aligner/final.mdl'
+        self.lexicon_fst_path = self.mfa_dir+'dictionary/'+self.pdict_name+'/L.fst'
+        self.lda_mat = self.mfa_dir+'pretrained_aligner/lda.mat'
+        
+        self.wav_scp = self.work+self.long_id+'_wav.scp' # files don't exist yet:
+        self.segments_scp = self.work+self.long_id+'_segments.scp'
+        self.text_int = self.work+self.long_id+'_text.int'
+        self.feats_scp = self.work+self.long_id+'_feats.scp'
+        self.fsts = self.work+self.long_id+'_fsts.ark'
+        self.likelihood = self.work+self.long_id+'_likelihoods.scp'
+        
+        self.utt2spk = self.work+self.long_id+'_utt2spk.scp' #! this does cmvn by utterance,
+        self.spk2utt = self.work+self.long_id+'_spk2utt.scp' # not by speaker (yet)
+        self.cmvn_ark = self.work+self.long_id+'_cmvn.ark'
+        self.cmvn_scp = self.work+self.long_id+'_cmvn.scp'
+        
+        self.mfcc_options = {'use-energy': False, 'frame-shift': 10, 'frame-length': 25, 'low-freq': 20, 'high-freq': 7800, 'sample-frequency': 16000, 'allow-downsample': True, 'allow-upsample': True, 'snip-edges': False}
+        self.feature_options = {'type': 'mfcc', 'use_energy': False, 'frame_shift': 10, 'frame_length': 25, 'snip_edges': False, 'low_frequency': 20, 'high_frequency': 7800, 'sample_frequency': 16000, 'allow_downsample': True, 'allow_upsample': True, 'uses_cmvn': True, 'uses_deltas': True, 'uses_voiced': False, 'uses_splices': True, 'uses_speaker_adaptation': True, 'use_pitch': False, 'min_f0': 50, 'max_f0': 500, 'delta_pitch': 0.005, 'penalty_factor': 0.1, 'splice_left_context': 3, 'splice_right_context': 3}
+        self.align_options = {'transition_scale': 1.0, 'acoustic_scale': 0.1, 'self_loop_scale': 0.1, 'beam': 10, 'retry_beam': 40, 'boost_silence': 1.0, 'optional_silence_csl': '1'}
+
+        self.phone_map = {}
+        self.setup()
+        
+    
+    def setup(self):
+        def temp_file(fpath,contents):
+            f = open(fpath,'w')
+            f.write(contents)
+            f.write('\n')
+            f.close()
+    
+        wav_scp_contents = self.long_id+' ' +self.wav_dir+self.speaker_id+'/'+self.long_id+'.wav'
+        temp_file(self.wav_scp, wav_scp_contents)
+        
+        segments_scp_contents = self.long_id + ' ' + self.long_id + ' 0.0 ' + self.duration
+        temp_file(self.segments_scp, segments_scp_contents)
+        
+        #! TODO change this for cmvn by speaker
+        temp_file(self.utt2spk,self.long_id+' '+self.long_id)
+        temp_file(self.spk2utt,self.long_id+' '+self.long_id)
+        
+        word_map_path = self.mfa_dir+'dictionary/'+self.pdict_name+'/words.txt'
+        with open(word_map_path,'r') as handle:
+            word_map = handle.read().splitlines()
+        word_map = [l.split(' ') for l in word_map]
+        word_map = {l[0]:l[1] for l in word_map}
+        
+        text_int_contents = self.long_id+' '+' '.join([word_map[word] for word in self.normalised_text.split(' ')])
+        temp_file(self.text_int, text_int_contents)
+        
+        with open(self.phone_map_path,'r') as handle:
+            self.phone_map = {l.split(' ')[1]:l.split(' ')[0] for l in handle.read().splitlines()}
+
+    
+    def process_intervals(self,intervals):
+        def cff(sc): # convert seconds to w2v2 feature-frame numbers
+            # controlling for float rounding problems...
+            return int(round( int(float(sc)*1000)*0.05))
+        assert len(intervals) > 0 #TODO don't crash if fail to align
+        
+        words = self.normalised_text.split(' ')
+        word_aligns = []
+        phone_aligns = defaultdict(list)
+        current_word_begin = None
+        w_ix = 0
+        p_ix = 0
+        for l in intervals:
+            start, dur, phone_int = l.split(' ')[2:]
+            phone_label = self.phone_map[phone_int]
+            start , end = cff(start), cff(float(start)+float(dur))
+            if phone_label == self.silence_phone:
+                continue
+            current_word_id = f'{w_ix:03d}__{words[w_ix]}'
+            phone, position = phone_label.rsplit("_",1)
+            if position in {"B", "S"}:
+                current_word_begin = start
+            phone_aligns[current_word_id].append((f'{p_ix:03d}__{phone}',start-current_word_begin,end-current_word_begin))
+            if position in {"E", "S"}:
+                word_aligns.append((current_word_id, current_word_begin, end))
+                current_word_begin = None
+                w_ix += 1
+            p_ix +=1
+        
+        for w,s,e in word_aligns: # !! TODO
+            assert e-s == phone_aligns[w][-1][2]
+   
+        return word_aligns, phone_aligns
+        
+        
+    def align(self): 
+    
+        def make_safe(value: typing.Any) -> str: # from MFA. TODO
+            if isinstance(value, bool):
+                return str(value).lower()
+            return str(value)
+            
+        def check_call(proc: subprocess.Popen):
+        #! from MFA. TODO
+            if proc.returncode is None:
+                proc.wait()
+            if proc.returncode != 0:
+                print('Kaldi Error.')
+    
+        with open(self.log_path, "w") as log_file:
+        
+        # compile training graphs:
+            graph_proc = subprocess.Popen(
+                [
+                    shutil.which("compile-train-graphs"),
+                    f"--read-disambig-syms={self.disambig_path}",
+                    self.tree_path,
+                    self.model_path,
+                    self.lexicon_fst_path,
+                    f"ark:{self.text_int}",
+                    f"ark:{self.fsts}",
+                ],
+                stderr=log_file,
+                encoding="utf8",
+                env=os.environ,
+            )
+            graph_proc.communicate()
+            
+            
+        # make mfccs
+            mfcc_base_command = [shutil.which("compute-mfcc-feats"), "--verbose=2"]
+            raw_ark_path = self.feats_scp.replace(".scp", ".ark")
+            for k, v in self.mfcc_options.items():
+                mfcc_base_command.append(f"--{k.replace('_', '-')}={make_safe(v)}")
+            
+            mfcc_base_command += ["ark:-", "ark:-"]
+            
+            seg_proc = subprocess.Popen(
+                [
+                    shutil.which("extract-segments"),
+                    f"scp:{self.wav_scp}",
+                    self.segments_scp,
+                    "ark:-",
+                ],
+                stdout=subprocess.PIPE,
+                stderr=log_file,
+                env=os.environ,
+            )
+            
+            mfcc_proc = subprocess.Popen(
+                mfcc_base_command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=seg_proc.stdout,
+                env=os.environ,
+            )  
+            
+            copy_proc = subprocess.Popen(
+                [
+                    shutil.which("copy-feats"),
+                    "--verbose=2",
+                    "--compress=true",
+                    "ark:-",
+                    f"ark,scp:{raw_ark_path},{self.feats_scp}",
+                ],
+                stdin=mfcc_proc.stdout,
+                stderr=log_file,
+                env=os.environ,
+                encoding="utf8",
+            )
+            
+            for line in mfcc_proc.stderr:
+                line = line.strip().decode("utf8")
+                log_file.write(line + "\n")
+            check_call(copy_proc) # TODO
+
+            subprocess.call(
+                [
+                    shutil.which("compute-cmvn-stats"),
+                    f"--spk2utt=ark:{self.spk2utt}",
+                    f"scp:{self.feats_scp}",
+                    f"ark,scp:{self.cmvn_ark},{self.cmvn_scp}",
+                ],
+                stderr=log_file,
+                env=os.environ,
+            )
+
+        # do the aligning
+            feat_string = f"ark,s,cs:apply-cmvn --utt2spk=ark:{self.utt2spk} scp:{self.cmvn_scp} scp:{self.feats_scp} ark:- |"
+            feat_string += f" splice-feats --left-context={self.feature_options['splice_left_context']} --right-context={self.feature_options['splice_right_context']} ark:- ark:- |"
+            feat_string += f" transform-feats {self.lda_mat} ark:- ark:- |"
+            align_proc = subprocess.Popen(
+                [
+                    shutil.which("gmm-align-compiled"),
+                    f"--transition-scale={self.align_options['transition_scale']}",
+                    f"--acoustic-scale={self.align_options['acoustic_scale']}",
+                    f"--self-loop-scale={self.align_options['self_loop_scale']}",
+                    f"--beam={self.align_options['beam']}",
+                    f"--retry-beam={self.align_options['retry_beam']}",
+                    "--careful=false",
+                    self.model_path,
+                    f"ark:{self.fsts}",
+                    feat_string,
+                    "ark:-",
+                    f"ark,t:{self.likelihood}",
+                ],
+                stdout=subprocess.PIPE,
+                stderr=log_file,
+                encoding="utf8",
+                env=os.environ,
+            )
+            
+            lin_proc = subprocess.Popen(
+                [
+                    shutil.which("linear-to-nbest"),
+                    "ark:-",
+                    f"ark:{self.text_int}",
+                    "",
+                    "",
+                    "ark:-",
+                ],
+                stdin=align_proc.stdout,
+                stdout=subprocess.PIPE,
+                stderr=log_file,
+                env=os.environ,
+            )
+            
+            align_words_proc = subprocess.Popen(
+                [
+                    shutil.which("lattice-align-words"),
+                    self.word_boundary_int,
+                    self.model_path,
+                    "ark:-",
+                    "ark:-",
+                ],
+                stdin=lin_proc.stdout,
+                stdout=subprocess.PIPE,
+                stderr=log_file,
+                env=os.environ,
+            )
+            
+            phone_proc = subprocess.Popen(
+                [
+                    shutil.which("lattice-to-phone-lattice"),
+                    self.model_path,
+                    "ark:-",
+                    "ark:-",
+                ],
+                stdout=subprocess.PIPE,
+                stdin=align_words_proc.stdout,
+                stderr=log_file,
+                env=os.environ,
+            )
+            nbest_proc = subprocess.Popen(
+                [
+                    shutil.which("nbest-to-ctm"),
+                    "--print-args=false",
+                    f"--frame-shift={round(self.feature_options['frame_shift']/1000,4)}",
+                    "ark:-",
+                    "-",
+                ],
+                stdin=phone_proc.stdout,
+                stderr=log_file,
+                stdout=subprocess.PIPE,
+                env=os.environ,
+                encoding="utf8",
+            )
+            
+            intervals = []
+            log_likelihood = None
+            for line in nbest_proc.stdout:
+                line = line.strip()
+                if not line:
+                    continue
+                intervals.append(line)
+            nbest_proc.wait()
+
+        word_aligns, phone_aligns = self.process_intervals(intervals)
+        return word_aligns, phone_aligns
+      
+        
+def makeAlign(exercise_text,rec_speaker_id,rec_file_id,rec_duration):
+    do_align = AlignOneFunction(exercise_text,rec_speaker_id,rec_file_id,rec_duration)
+    return do_align.align()
+    
+
+
+
---
+++ captiniscore.py
@@ -0,0 +1,157 @@
+import numpy as np
+import soundfile as sf
+from scipy import signal
+from collections import defaultdict
+from dtw import dtw
+import glob, torch, transformers, sys, random, pickle
+transformers.logging.set_verbosity(transformers.logging.ERROR)
+
+
+class PronunciationScorer():
+
+    def __init__(self, wav_dir, reference_feat_dir, model_path, model_layer): 
+
+        self.reference_feat_dir = reference_feat_dir
+        self.model_path = model_path
+        self.model_layer = model_layer
+        self.new_wav_dir = wav_dir
+
+        self.featurize = self.w2v2_featurizer()
+                
+    # return a function from wav file path to speech embedding
+    # see: https://github.com/Bartelds/neural-acoustic-distance/blob/main/extract_features.py
+    def w2v2_featurizer(self):
+        model_kwargs = {"num_hidden_layers": self.model_layer} # !! TODO
+        model = transformers.models.wav2vec2.Wav2Vec2Model.from_pretrained(self.model_path, **model_kwargs)
+        model.eval()
+        if torch.cuda.is_available():
+            model.cuda()
+    
+        @torch.no_grad()
+        def _featurizer(wav_path):
+            wav, sr = sf.read(wav_path, dtype=np.float32)
+            if len(wav.shape) == 2:
+                wav = wav.mean(1)
+            if sr != 16_000:
+                new_length = int(wav.shape[0] / sr * 16_000)
+                wav = signal.resample(wav, new_length)
+            
+            wav = torch.from_numpy(wav).unsqueeze(0)
+            if torch.cuda.is_available():
+                wav = wav.cuda()
+            hidden_state = model(wav).last_hidden_state.squeeze(0).cpu().numpy()
+            return hidden_state
+
+        return _featurizer
+        
+        
+    def score_one(self,exercise_id,test_speaker,test_file,word_aligns,phone_aligns):
+    
+        reference_path = f'{self.reference_feat_dir}exercise_{exercise_id}.pickle'
+        with open(reference_path,'rb') as handle:
+            reference_sets = pickle.load(handle)
+        assert phone_aligns.keys() == reference_sets['L1'].keys()
+                
+        test_wav_path = f'{self.new_wav_dir}{test_speaker}/{test_speaker}-{test_file}.wav'
+        test_feats = self.featurize(test_wav_path)
+        test_word_feats = {w:test_feats[s:e] for w,s,e in word_aligns}
+        
+        
+        def dtw_function (test_word,references_words):
+            references_words = [v for v in references_words]
+            def _dtw(a_feats,b_feats):
+                if a_feats.shape[0] < 2 or b_feats.shape[0] < 2:
+                    return np.nan
+                return dtw(a_feats,b_feats,keep_internals=True)
+            return [_dtw(test_word,ref_word) for ref_word in references_words]
+        
+        
+        def clean(id):
+            return id.split('__',1)[1]
+        
+        
+        # average dtw cost per word, relative to reference set
+        def get_word_avg(dtw_obj_list):
+            # the only possible float here is numpy.nan
+            # but numpy.isnan() cannot check DTW objects
+            # therefore, discard all floats, remaining objects are DTW objects
+            dtw_obj_list = [d for d in dtw_obj_list if not isinstance(d,float)]
+            if len(dtw_obj_list) == 0:
+                return(np.nan)
+            else:
+                return np.nanmean([d.normalizedDistance for d in dtw_obj_list])
+
+
+        # average cost per phone relative to reference set
+        def get_phone_avg(dtw_obj_list,word_phone_aligns):
+            def _get_pcost(pstart,pend,dtw_obj):
+                st = np.where(dtw_obj.index1==pstart)[0][0]
+                en = np.where(dtw_obj.index1==min(max(dtw_obj.index1),max(pend-1,0)))
+                en = en[0][-1]+1 #add 1 for slicing
+                phone_tokens = zip(dtw_obj.index1[st:en],dtw_obj.index2[st:en])
+                return np.mean([dtw_obj.localCostMatrix[tx,rx] for tx,rx in phone_tokens])
+                
+            dtw_obj_list = [d for d in dtw_obj_list if not isinstance(d,float)]
+            if len(dtw_obj_list) == 0:
+                return { ph : np.nan for ph,s,e in word_phone_aligns}
+            
+            pcosts = {ph : (np.nanmean([_get_pcost(s,e,d) for d in dtw_obj_list]) if (e-s > 1) else np.nan) for ph,s,e in word_phone_aligns}
+            return pcosts
+        
+        
+        # heuristic score:
+        # native speakers should always be closest to other native speakers
+        # l2 speakers may be closer to l2 or similar distance from both groups    
+        def prepare_score(l1,l2):
+            if np.isnan(l1) or np.isnan(l2):
+                return('TOO SHORT TO SCORE')
+            else:
+                return round(100*(l2-l1)/(l2+l1),2)     
+            
+            
+        def _compare (comp_set): # call with 'L1' or 'L2' 
+        
+            word_dtws = { word_id : 
+                dtw_function(
+                    test_word_feats[word_id], 
+                    reference_sets[comp_set][word_id].values()) 
+                for word_id,s,e 
+                in word_aligns}
+                
+            word_avg_costs = { word_id : 
+                get_word_avg(word_dtws[word_id]) 
+                for word_id,s,e 
+                in word_aligns }
+                
+            phone_avg_costs = { word_id : 
+                get_phone_avg(word_dtws[word_id], phone_aligns[word_id]) 
+                for word_id,s,e 
+                in word_aligns }
+                
+            return word_avg_costs, phone_avg_costs
+          
+        
+        
+        l1_w_costs, l1_p_costs = _compare('L1')
+        l2_w_costs, l2_p_costs = _compare('L2')
+        
+        final_word_scores = [
+            (clean(w_id), prepare_score(l1_w_costs[w_id],l2_w_costs[w_id]))
+            for w_id,s,e 
+            in word_aligns
+        ]
+        
+        final_phone_scores = [
+            (clean(w_id), [(
+                clean(p_id), 
+                prepare_score(l1_p_costs[w_id][p_id], l2_p_costs[w_id][p_id])) 
+                for p_id,s,e 
+                in phone_aligns[w_id]
+                ])
+            for w_id, s,e 
+            in word_aligns
+        ]
+        
+        return final_word_scores, final_phone_scores
+        
+        
